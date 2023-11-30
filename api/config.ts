0ccbfdbda87e925cd8172fd1222070cb7c9c0c09
Beloved1310 <adeoluwafisayomi@gmail.com>
2023-11-30 01:16:56 +0100

    feat/add-products-endpoint

--- api/config.ts
+++ api/config.ts
@@ -14,5 +14,8 @@ export const config: IConfig = {
   MONGODBURI: <string>env.MONGODBURI,
   REDIS_HOST: <string>env.REDIS_HOST,
   REDIS_PASSWORD: <string>env.REDIS_PASSWORD,
+  CLOUDINARY_CLOUD_NAME: <string>env.CLOUDINARY_CLOUD_NAME,
+  CLOUDINARY_API_SECRET: <string>env.CLOUDINARY_API_SECRET,
+  CLOUDINARY_API_KEY:  <string>env.CLOUDINARY_API_KEY,
   REDIS_PORT: parseInt(env.REDIS_PORT!)
 };
--- api/interfaces/iConfig.ts
+++ api/interfaces/iConfig.ts
@@ -5,4 +5,7 @@ export interface IConfig {
     REDIS_HOST: string;
     REDIS_PASSWORD: string;
     REDIS_PORT: number;
+    CLOUDINARY_CLOUD_NAME: string;
+    CLOUDINARY_API_KEY: string;
+    CLOUDINARY_API_SECRET: string
   }
