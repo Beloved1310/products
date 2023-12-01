76a71c718613397332847aab83ff2569710215fb
Beloved1310 <adeoluwafisayomi@gmail.com>
2023-12-01 02:19:07 +0100

    feat/add-auth

--- api/routes/product.ts
+++ api/routes/product.ts
@@ -1,13 +1,14 @@
 import express from 'express'
 import { asyncErrorhandling } from '../middleware/async'
 import { productController } from '../controller/product'
+import {auth} from '../middleware/auth'
 
 const router = express.Router()
 
-router.post('/create', asyncErrorhandling(productController.createProduct))
-router.put('/update/:id', asyncErrorhandling(productController.updateProduct))
-router.get('/:id', asyncErrorhandling(productController.viewProduct))
-router.get('/', asyncErrorhandling(productController.listProducts))
-router.delete('/:id', asyncErrorhandling(productController.deleteProduct))
+router.post('/create', auth, asyncErrorhandling(productController.createProduct))
+router.put('/update/:id',auth,  asyncErrorhandling(productController.updateProduct))
+router.get('/:id', auth, asyncErrorhandling(productController.viewProduct))
+router.get('/',auth, asyncErrorhandling(productController.listProducts))
+router.delete('/:id',auth, asyncErrorhandling(productController.deleteProduct))
 
 export default router
